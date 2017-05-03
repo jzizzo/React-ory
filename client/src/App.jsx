5244b387688217e16205812022309871fd4c0acd
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-05-03 16:10:08 -0700

    initial repo with basic client and server created and connected

---
+++ client/src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+
+
+  render () {
+
+    return (
+      <div>
+        <h1>Welcome to React-ory</h1>
+      </div>
+    );
+  }
+
+}
+
+export default App;
