cc00bf7330975214d254f09e085fb4b608f61a37
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-05-15 11:29:00 -0700

    add toggle functionality to hide and show additional student details

--- client/src/StudentEntry.jsx
+++ client/src/StudentEntry.jsx
@@ -3,16 +3,22 @@ import React, { Component } from 'react';
 class StudentEntry extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      displayStudentDetails: false
+    };
   }
 
-  render() {
+  toggleStudentDetails() {
+    this.setState({
+      displayStudentDetails: !this.state.displayStudentDetails
+    });
+  }
+
+  displayStudentDetails() {
     const { student } = this.props;
+
     return (
       <div>
-      <br />
-        <li>
-          {student.name}
-        </li>
         <br />
         {student.beforeHR}
         <br />
@@ -24,6 +30,19 @@ class StudentEntry extends Component {
         <br />
         {student.randomFact}
         <br />
+      </div>
+    );
+  }
+
+  render() {
+    const { student } = this.props;
+    return (
+      <div>
+      <br />
+        <li onClick={this.toggleStudentDetails.bind(this)}>
+          {student.name}
+        </li>
+        {this.state.displayStudentDetails === true ? this.displayStudentDetails() : null }
       </div>
     );
   }
