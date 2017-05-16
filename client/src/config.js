36f3ff14ca50b23937d8670090e4a8a0a5f3df30
Joseph Zizzo <zizzo.joseph.k@gmail.com>
2017-05-16 10:44:40 -0700

    remove config copy

--- client/src/config.js
+++
@@ -1,8 +0,0 @@
-var config = {
-  apiKey: 'AIzaSyCdT7fIeaT_iwxCKjCvcbWR5LmL_pbD3BI',
-  authDomain: 'react-ory.firebaseapp.com',
-  databaseURL: 'https://react-ory.firebaseio.com',
-  projectId: 'react-ory',
-  storageBucket: 'react-ory.appspot.com',
-  messagingSenderId: '130689649897'
-};
